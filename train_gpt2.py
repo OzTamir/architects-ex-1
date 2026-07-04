30d5d97b74742f12eaaafa447ab2a0cb5d900bc8
Oz Tamir <oz@tamir.fun>
2026-07-04 18:56:33 +0300

    feat: add torch.compile (1.2.3)

--- train_gpt2.py
+++ train_gpt2.py
@@ -316,10 +316,14 @@ torch.set_float32_matmul_precision('high')
 model = GPT(GPTConfig(vocab_size=50304))
 # model = GPT.from_pretrained("gpt2") # or init from OpenAI GPT-2
 model.to(device)
+
+# Compile the model!
+model = torch.compile(model)
+
 max_lr = 6e-4
 min_lr = max_lr * 0.1
 warmup_steps = 715
-max_steps = 500 # 19,073 steps is ~1 epoch, if data is 10B tokens and batch size 0.5M tokens
+max_steps = 1000 # With the new optimization, we can train for more steps
 
 def get_lr(it):
     # 1) linear warmup for warmup_iters steps
